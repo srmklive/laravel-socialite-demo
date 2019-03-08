018c83e44e28e7940f4ad2c232b4f5cb149885d3
Raza Mehdi <srmk@outlook.com>
2019-03-08 10:48:35 +0000

    Add auth scaffolding

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 Route::get('{provider}/redirect', 'Socialite\RedirectController@index');
 Route::get('{provider}/callback', 'Socialite\CallbackController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
